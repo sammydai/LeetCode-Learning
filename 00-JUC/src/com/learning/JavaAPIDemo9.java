8351f08a0444416da7f0dfc29bf08bf22bec838a
sammy <sammydaiwenting@163.com>
2019-05-07 21:52:07 +0800

    initial commit

--- 00-JUC/src/com/learning/JavaAPIDemo9.java
+++ 00-JUC/src/com/learning/JavaAPIDemo9.java
@@ -9,12 +9,6 @@ import java.util.concurrent.TimeUnit;
 
 public class JavaAPIDemo9 {
      public static void main(String[] args) {
-        ExecutorService service = Executors.newFixedThreadPool(3); // 线程池只能装下3个人
-        for (int x = 0; x < 10; x++) {
-            service.submit(()->{ //线程池会负责启动
-                System.out.println(Thread.currentThread().getName()+"执行操作");
-            });
-        }
-        service.shutdown(); //线程池执行完毕后关掉
+     
     }
 }
